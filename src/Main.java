e692af78320c1c66b3870f543a6027619697a178
RafetErsoy <rafet.ersoy.cs@gmail.com>
2022-09-16 15:21:48 +0300

    The main file is complete.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        Instructor instructor = new Instructor(
+                1,
+                "ins@email.com",
+                1,
+                "Engin",
+                "Demirog",
+                "11111111111",
+                "Expert");
 
+        UserManager userManager = new UserManager();
+        userManager.add(instructor); // Sadece user tablosuna ekleme yapiyor.
+
+        InstructorManager instructorManager = new InstructorManager();
+        instructorManager.add(instructor); // User tipimiz instructor oldugu için ilgili tum tablolara ekleme yapiyor.
     }
 }
