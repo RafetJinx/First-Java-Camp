d49c78589d5c2a10a5fc8442d24fda93e4371d2d
RafetErsoy <rafet.ersoy.cs@gmail.com>
2022-09-17 18:21:16 +0300

    Forgotten base class added

---
+++ Week 4/Homework_2/src/Entities/Customer.java
@@ -0,0 +1,10 @@
+package Entities;
+
+import Abstract.Entity;
+
+public class Customer implements Entity {
+    // Siniflar ciplak kaliyor ise yani inheritence veya implementation almiyor ise
+    // ilerleyen zamanlarda nesnellik zaafiyeti yasanabilecek demektir.
+
+
+}
