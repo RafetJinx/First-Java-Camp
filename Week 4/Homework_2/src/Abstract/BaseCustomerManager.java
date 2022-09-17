3d5c369947b3e21db1dcb598a5ed59b6202214ac
RafetErsoy <rafet.ersoy.cs@gmail.com>
2022-09-17 18:21:47 +0300

    Manager classes created.

---
+++ Week 4/Homework_2/src/Abstract/BaseCustomerManager.java
@@ -0,0 +1,14 @@
+package Abstract;
+
+import Entities.Customer;
+
+public abstract class BaseCustomerManager implements CustomerService{
+    // BaseCustomerManager'in abstract class olarak olusturulmasinin sebebi:
+    //      Kod implementasyonu olacak (birden fazla yerde ayni kod blogu kullanilacak)
+    //      Bu durumda kod implementasyonunu yonetmek icin abstract class olusturuyoruz.
+    //      Boylece "BaseCustomerManager"i extend eden siniflar icin ayni islevi gorecek.
+    @Override
+    public void save(Customer customer) {
+
+    }
+}
