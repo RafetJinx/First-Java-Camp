09d703231f5d27f3f6df815b1d9766ec3d3a6af1
RafetErsoy <rafet.ersoy.cs@gmail.com>
2022-09-18 20:00:06 +0300

    Modified and completed Main.java

--- Week 4/Homework_2/src/Main.java
+++ Week 4/Homework_2/src/Main.java
@@ -1,2 +1,23 @@
+import Abstract.BaseCustomerManager;
+import Adapters.MernisServiceAdapter;
+import Concrete.CustomerCheckManager;
+import Concrete.NeroCustomerManager;
+import Concrete.StarbucksCustomerManager;
+import Entities.Customer;
+
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        // IoC Container ile kontrol edilir (ex: BaseCustomer Manager)
+        BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+        customerManager.save(new Customer(
+                1,
+                "rafet",
+                "ersoy",
+                LocalDate.of(2000,2,12),
+                "56389321984"));
+
+        System.out.println();
+    }
 }
