3ae54e4bccedb5523f12da8d505f9b3d57d926d6
RafetErsoy <rafet.ersoy.cs@gmail.com>
2022-09-24 18:31:04 +0300

    Entity classes created and filled with constructor and getter & setter methods.

---
+++ Week 4/Homework_3/src/Entities/Campaign.java
@@ -0,0 +1,64 @@
+package Entities;
+
+import java.time.LocalDate;
+
+import Abstract.Entity;
+
+public class Campaign implements Entity{
+	private int id;
+	private String category;
+	private String name;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	
+	public Campaign(int id, String category, String name, LocalDate startDate, LocalDate endDate) {
+		super();
+		this.id = id;
+		this.category = category;
+		this.name = name;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+	
+	
+}
