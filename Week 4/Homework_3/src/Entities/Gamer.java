abcabdd4a8edd23d7d62c8c446aa3293cb43a5a5
RafetErsoy <rafet.ersoy.cs@gmail.com>
2022-09-25 19:35:45 +0300

    Gamer.java has been created.

--- Week 4/Homework_3/src/Entities/Gamer.java
+++ Week 4/Homework_3/src/Entities/Gamer.java
@@ -2,62 +2,22 @@ package Entities;
 
 import java.time.LocalDate;
 
-public class Gamer extends Customer{
-	private int id;
-	private String firstName;
-	private String lastName;
-	private LocalDate dateOfBirth;
-	private String nationalityId;
+public class Gamer extends IndividualCustomer{
 	
+	private String interest;
+
 	public Gamer(int customerId, String email, String contactNumber, int gamerId, String firstName, String lastName,
-			LocalDate dateOfBirth, String nationalityId) {
-		super(customerId, email, contactNumber);
-		this.id = gamerId;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.dateOfBirth = dateOfBirth;
-		this.nationalityId = nationalityId;
+			LocalDate dateOfBirth, String nationalityId, String interest) {
+		super(customerId, email, contactNumber, gamerId, firstName, lastName, dateOfBirth, nationalityId);
+		this.interest = interest;
 	}
 
-	public int getId() {
-		return id;
+	public String getInterest() {
+		return interest;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public void setInterest(String interest) {
+		this.interest = interest;
 	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public LocalDate getDateOfBirth() {
-		return dateOfBirth;
-	}
-
-	public void setDateOfBirth(LocalDate dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
-
-	public String getNationalityId() {
-		return nationalityId;
-	}
-
-	public void setNationalityId(String nationalityId) {
-		this.nationalityId = nationalityId;
-	}
-	
 	
 }
