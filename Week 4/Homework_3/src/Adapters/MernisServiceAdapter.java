f69b5c9c8aced607a8ef8dd044d99a412f4a344d
RafetErsoy <rafet.ersoy.cs@gmail.com>
2022-09-25 19:46:19 +0300

    MernisServiceAdapter has been added.

--- Week 4/Homework_3/src/Adapters/MernisServiceAdapter.java
+++ Week 4/Homework_3/src/Adapters/MernisServiceAdapter.java
@@ -2,22 +2,22 @@ package Adapters;
 
 import java.rmi.RemoteException;
 
-import Abstract.GamerCheckService;
-import Entities.Gamer;
+import Abstract.IndividualCustomerCheckService;
+import Entities.IndividualCustomer;
 import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
 
-public class MernisServiceAdapter implements GamerCheckService{
+public class MernisServiceAdapter implements IndividualCustomerCheckService{
 
 	@Override
-	public boolean checkIfRealPerson(Gamer gamer){
+	public boolean checkIfRealPerson(IndividualCustomer individualCustomer){
 		KPSPublicSoapProxy kpsPublicSoapProxy = new KPSPublicSoapProxy();
 		boolean result = false;
 		try {
 			result = kpsPublicSoapProxy.TCKimlikNoDogrula(
-					Long.parseLong(gamer.getNationalityId()), 
-					gamer.getFirstName().toUpperCase(), 
-					gamer.getLastName().toUpperCase(), 
-					gamer.getDateOfBirth().getYear());
+					Long.parseLong(individualCustomer.getNationalityId()), 
+					individualCustomer.getFirstName().toUpperCase(), 
+					individualCustomer.getLastName().toUpperCase(), 
+					individualCustomer.getDateOfBirth().getYear());
 		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
